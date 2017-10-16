4be3097441a05f51809db8165cb8d077469038e8
Eva Dinckel <evadinckel@gmail.com>
2017-10-16 19:14:08 +0100

    commit on step5

---
+++ directory_step5.rb
@@ -0,0 +1,32 @@
+students = [
+"Dr. Hannibal Lecter",
+"Darth Vader",
+"Nurse Ratched",
+"Michael Corleone",
+"Alex DeLarge",
+"The Wicked Witch of the West",
+"Terminator",
+"Freddy Krueger",
+"The Joker",
+"Joffrey Baratheon",
+"Norman Bates"
+]
+
+def print_header
+  puts "The Students of Villains Academy"
+  puts "--------------------------------------"
+end
+
+def print(names)
+  names.each do |name|
+    puts name
+  end
+end
+
+def print_footer(names)
+  puts "Overall, we have #{names.count} great students"
+end
+
+print_header
+print (students)
+print_footer(students)
