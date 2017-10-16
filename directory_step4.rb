84fe88f50778d7edba2017109ef0d3dbdebbfebe
Eva Dinckel <evadinckel@gmail.com>
2017-10-16 19:08:13 +0100

    commit on step4

---
+++ directory_step4.rb
@@ -0,0 +1,23 @@
+
+
+students = [
+"Dr. Hannibal Lecter",
+"Darth Vader",
+"Nurse Ratched",
+"Michael Corleone",
+"Alex DeLarge",
+"The Wicked Witch of the West",
+"Terminator",
+"Freddy Krueger",
+"The Joker",
+"Joffrey Baratheon",
+"Norman Bates"
+]
+
+puts "The students of Villain Academy"
+puts "-------------------------------"
+students.each do |student|
+  puts student
+end
+
+puts "Overall, we have #{students.count} great students"
